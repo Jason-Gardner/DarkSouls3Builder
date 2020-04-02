9ee373eda5884fea393fd7b2b4be7db1d7ffa671
Jason Gardner <jasonagardner@gmail.com>
2020-04-02 09:59:12 -0400

    Initial commit.

---
+++ DarkSouls3/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function updateWeapon(item) {
+
+}
+
+function updateArmor(item) {
+
+}
+
+function updateRing(item) {
+
+}
+
+function newClass(myClass) {
+    var x = document.getElementById("class").value;
+
+    $.ajax({
+        url: '/Home/Character',
+        data: { class: x },
+        success: function (data) {}
+        }); 
+}
